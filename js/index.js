38f59e6bf6c75b7734a7356bce06251990b784aa
freelancerjolil <freelancerjolil@gmail.com>
2024-09-23 22:15:15 +0600

    All border removed and Complete Font Part Design

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 function goToPage() {
   window.location.href = "faq.html"; // Replace with the HTML page you want to navigate to
 }
+
+// Get the navbar
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 100) {
+    header.classList.add("bg-white", "shadow-lg", "backdrop-blur-md");
+    header.classList.remove("bg-transparent", "backdrop-blur-0");
+  } else {
+    header.classList.add("bg-transparent", "backdrop-blur-0");
+    header.classList.remove("bg-white", "shadow-lg", "backdrop-blur-md");
+  }
+});
