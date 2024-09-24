5c34f943d67a7b1eabe4f88a20735e65a1c12e3a
freelancerjolil <freelancerjolil@gmail.com>
2024-09-24 18:12:38 +0600

    Modad Added

--- js/index.js
+++ js/index.js
@@ -13,23 +13,29 @@ window.addEventListener("scroll", function () {
 });
 
 // Donate Button 1
-const donateNowButton = document.getElementById("donate-now");
 let mainBalance = document.getElementById("main-balance");
-
-let totalDonation = 0;
-
+const donateModal = document.getElementById("donateModal");
+const donateNowButton = document.getElementById("donate-now");
+let totalDonation1 = 0;
 donateNowButton.addEventListener("click", function () {
-  const inputbox = parseFloat(document.getElementById("input-box1").value);
-  if (!isNaN(inputbox) && inputbox > 0) {
-    totalDonation += inputbox;
-    const mainAmount = parseFloat(mainBalance.innerText);
-    mainBalance.innerText = (mainAmount - totalDonation).toFixed(2);
-    const donateBox1 = document.getElementById("donate-box-1");
-    donateBox1.innerText = totalDonation.toFixed(2);
+  const inputbox1 = parseFloat(document.getElementById("input-box1").value);
+  if (!isNaN(inputbox1) && inputbox1 > 0) {
+    document.getElementById(
+      "modalDonationAmount"
+    ).innerText = `Amount: $${inputbox1.toFixed(2)}`;
+    donateModal.showModal();
+
+    document.getElementById("confirmDonation").onclick = function () {
+      totalDonation1 += inputbox1;
+      const mainAmount = parseFloat(mainBalance.innerText);
+      mainBalance.innerText = (mainAmount - inputbox1).toFixed(2);
+      const donateBox1 = document.getElementById("donate-box-1");
+      donateBox1.innerText = totalDonation1.toFixed(2);
+      donateModal.close();
+    };
   } else {
     alert("Please enter a valid number greater than 0");
   }
-
   document.getElementById("input-box1").value = "";
 });
 
@@ -39,34 +45,46 @@ let totalDonation2 = 0;
 donateBtn2.addEventListener("click", function () {
   const inputbox2 = parseFloat(document.getElementById("input-box2").value);
   if (!isNaN(inputbox2) && inputbox2 > 0) {
-    totalDonation2 += inputbox2;
-    const mainAmount = parseFloat(mainBalance.innerText);
-    mainBalance.innerText = (mainAmount - inputbox2).toFixed(2);
-    const donateBox2 = document.getElementById("donate-box-2");
-    donateBox2.innerText = totalDonation2.toFixed(2);
+    document.getElementById(
+      "modalDonationAmount"
+    ).innerText = `Amount: $${inputbox2.toFixed(2)}`;
+    donateModal.showModal();
+    document.getElementById("confirmDonation").onclick = function () {
+      totalDonation2 += inputbox2;
+      const mainAmount = parseFloat(mainBalance.innerText);
+      mainBalance.innerText = (mainAmount - inputbox2).toFixed(2);
+      const donateBox2 = document.getElementById("donate-box-2");
+      donateBox2.innerText = totalDonation2.toFixed(2);
+      donateModal.close();
+    };
   } else {
     alert("Please enter a valid number");
   }
   document.getElementById("input-box2").value = "";
 });
-
-// Donate Button 3
 const donateBtn3 = document.getElementById("donateBtn3");
 let totalDonation3 = 0;
+
 donateBtn3.addEventListener("click", function () {
   const inputbox3 = parseFloat(document.getElementById("input-box3").value);
   if (!isNaN(inputbox3) && inputbox3 > 0) {
-    totalDonation3 += inputbox3;
-    const mainAmount = parseFloat(mainBalance.innerText);
-    mainBalance.innerText = (mainAmount - inputbox3).toFixed(2);
-    const donateBox3 = document.getElementById("donate-box-3");
-    donateBox3.innerText = totalDonation3.toFixed(2);
+    document.getElementById(
+      "modalDonationAmount"
+    ).innerText = `Amount: $${inputbox3.toFixed(2)}`;
+    donateModal.showModal();
+    document.getElementById("confirmDonation").onclick = function () {
+      totalDonation3 += inputbox3;
+      const mainAmount = parseFloat(mainBalance.innerText);
+      mainBalance.innerText = (mainAmount - inputbox3).toFixed(2);
+      const donateBox3 = document.getElementById("donate-box-3");
+      donateBox3.innerText = totalDonation3.toFixed(2);
+      donateModal.close();
+    };
   } else {
-    alert("Please enter a valid number greater than 0");
+    alert("Please enter a valid number");
   }
   document.getElementById("input-box3").value = "";
 });
-
 // Toogle Button Function
 const historyTab = document.getElementById("history-tab");
 const donationTab = document.getElementById("donation-tab");
